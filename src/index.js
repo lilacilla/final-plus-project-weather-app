abbce4239d48719b11b9f0ec8d97c32cda59b23a
lilacilla <149900581+lilacilla@users.noreply.github.com>
2023-11-13 23:15:42 +0000

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function updateWeatherInformation(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "4c8c7b09546det2d72f3521aa5o882ea";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInformation);
+}
+
 function initiateSearchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", initiateSearchForm);
+
+searchCity("London");
